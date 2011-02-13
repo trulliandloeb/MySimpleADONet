c875337306dde855c93d1cb7f5dd3bc562c93cfe
trulliandloeb <trulliandloeb@hotmail.com>
2011-02-13 16:16:51 +0800

    add scalar function

---
+++ SimpleAdoNet/SimpleAdoNet/EasySQLer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace SimpleAdoNet
+{
+    public class EasySQLer
+    {
+        private string connectionString;
+        private string sql;
+        private SQLiteCommand command;
+
+        public EasySQLer(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public EasySQLer Query(string sql)
+        {
+            this.sql = sql;
+            return this;
+        }
+
+        public EasySQLer FillParameters(Action<SQLiteParameterCollection> fillAction)
+        {
+            command = new SQLiteCommand(sql);
+            fillAction(command.Parameters);
+            return this;
+        }
+
+        public object Scalar()
+        {
+            object result = null;
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                if (command == null)
+                {
+                    command = new SQLiteCommand(sql);
+                }
+                command.Connection = conn;
+                try
+                {
+                    conn.Open();
+                    result = command.ExecuteScalar();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ SimpleAdoNet/SimpleAdoNet/Test/EasySQLerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SimpleAdoNet.Test
+{
+    [TestFixture]
+    public class EasySQLerTest
+    {
+        private const string CONN_STRING = "data source=\"D:\\Program Files (x86)\\SQLite.NET\\db\\mysqlo\"";
+
+        [Test]
+        public void TestScalar()
+        {
+            var sqler = new EasySQLer(CONN_STRING);
+            var totalCount = sqler.Query("select count(*) from USER_ACCOUNT").Scalar();
+            Assert.AreEqual(2, totalCount);
+        }
+    }
+}
